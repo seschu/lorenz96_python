a40dd3d3b8cfcfd0f79d2d9e178941ca3fd7d059
Sebastian Schubert <sebastian.schubert@uni-hamburg.de>
2017-06-15 12:44:21 +0200

    test

--- lorenz96_compute_secondary_instabilities.py
+++ lorenz96_compute_secondary_instabilities.py
@@ -11,10 +11,10 @@ savename='secondaryinstabilities_v3'
 paraL96=np.load(savename+"/paraL96.npy")
 paraL96=paraL96[()]
 # M number exponents
-M = paraL96['dimX'] #+ paraL96['dimX']*paraL96['dimY'] # -1 full spectrum
-dimN = paraL96['dimX']# + paraL96['dimX']*paraL96['dimY'] # -1 full spectrum
+M = paraL96['dimX'] + paraL96['dimX']*paraL96['dimY'] # -1 full spectrum
+dimN = paraL96['dimX'] + paraL96['dimX']*paraL96['dimY'] # -1 full spectrum
 
-steplengthforsecondorder = 10
+steplengthforsecondorder = [0, 1, 5, 10]
 
 
 t = np.load(savename+'/t.npy')
@@ -26,11 +26,11 @@ lyapmean_clv = np.memmap(savename+'/lyapmean_clv.dat',mode='r',shape=(M,1),dtype
 lyaploc_clv = np.memmap(savename+'/lyaploc_clv',mode='r',shape=(len(t),M,1),dtype='float64')
 lyaploc_blv = np.memmap(savename+'/lyaploc_blv',mode='r',shape=(len(t)-1,M,1),dtype='float64')
 
-secondary_vector = np.memmap(savename+'/secondary_vector.dat',mode='w+',shape=(len(t),dimN,M,steplengthforsecondorder),dtype='float64')
-contracted_CLVs = np.memmap(savename+'/contracted_clvs.dat',mode='w+',shape=(len(t),dimN,M,steplengthforsecondorder),dtype='float64') #(final_clv,init_clv)
-solution = np.memmap(savename+'/solution.dat',mode='w+',shape=(len(t),dimN,M,steplengthforsecondorder),dtype='float64')
-normalized_solution = np.memmap(savename+'/solution.dat',mode='w+',shape=(len(t),dimN,M,steplengthforsecondorder),dtype='float64')
-growth = np.memmap(savename+'/growth.dat',mode='w+',shape=(len(t),M,steplengthforsecondorder),dtype='float64')
+#secondary_vector = np.memmap(savename+'/secondary_vector.dat',mode='w+',shape=(len(t),dimN,M,len(steplengthforsecondorder)),dtype='float64')
+contracted_CLVs = np.memmap(savename+'/contracted_clvs.dat',mode='w+',shape=(len(steplengthforsecondorder),len(t),dimN,M),dtype='float64') #(final_clv,init_clv)
+solution = np.memmap(savename+'/solution.dat',mode='w+',shape=(len(steplengthforsecondorder),len(t),dimN,M),dtype='float64')
+normalized_solution = np.memmap(savename+'/solution.dat',mode='w+',shape=(len(steplengthforsecondorder),len(t),dimN,M),dtype='float64')
+growth = np.memmap(savename+'/growth.dat',mode='w+',shape=(len(steplengthforsecondorder),len(t),M),dtype='float64')
 invCLV = np.memmap(savename+'/invCLV.dat',mode='r',shape=(len(t),dimN,M,1),dtype='float64')
 
 #for tn, (ts,te) in enumerate(zip(t[0:-1],t[1:])):
@@ -42,20 +42,20 @@ invCLV = np.memmap(savename+'/invCLV.dat',mode='r',shape=(len(t),dimN,M,1),dtype
 for tn, (ts,te) in enumerate(zip(t[0:-20],t[1:-19])):
     dtau=te -ts
     print(tn)
-    for n_step in range(0,steplengthforsecondorder):
-        print(tn,n_step)
-        contracted_CLVs[tn,:,:,n_step]=1/2*l96.contract_hess_l96_1layer_v2(invCLV[tn+n_step,:,:,0],CLV[tn,:,:,0])
+    for n_step, len_step in enumerate(steplengthforsecondorder):
+        print(tn,len_step )
+        contracted_CLVs[n_step,tn,:,:]=1/2*l96.contract_hess_l96_2layer_v2(invCLV[tn+len_step ,:,:,0],CLV[tn,:,:,0],paraL96)
         # multiply with growth factor   
-        #for final_clv,init_clv in product(np.arange(0,M),np.arange(0,M)):
         for step in range(0,n_step+1):
-            solution[tn,:,:,n_step] = solution[tn,:,:,n_step] + (
-            dtau * np.multiply(np.exp(np.sum(dtau*lyaploc_clv[tn+step:tn+n_step,:,np.newaxis,0], axis =0)),np.multiply(contracted_CLVs[tn,:,:,n_step],np.exp(2*np.sum(dtau*lyaploc_clv[tn:tn+step,np.newaxis,:,0], axis =0))))
+            solution[n_step,tn,:,:] = solution[n_step,tn,:,:] + (
+            dtau * np.multiply(np.exp(np.sum(dtau*lyaploc_clv[tn+step:tn+len_step,:,np.newaxis,0], axis =0)),np.multiply(contracted_CLVs[n_step,tn,:,:],np.exp(2*np.sum(dtau*lyaploc_clv[tn:tn+step,np.newaxis,:,0], axis =0))))
             )
-        growth[tn,:,n_step]=np.linalg.norm(np.matmul(CLV[tn+n_step,:,:,0],solution[tn,:,:,n_step]),axis = 0)
-        growth[tn,:,n_step]=np.divide(growth[tn,:,n_step],growth[tn,:,0])        
-        normalized_solution[tn,:,:,n_step]=np.divide(solution[tn,:,:,n_step],growth[tn,np.newaxis,:,n_step])
-    if tn % 100 == 0:
+        growth[n_step,tn,:]=np.linalg.norm(np.matmul(CLV[tn+len_step ,:,:,0],solution[n_step, tn,:,:]),axis = 0)
+        growth[n_step,tn,:]=np.divide(growth[n_step,tn,:],growth[0,tn,:])        
+        normalized_solution[n_step,tn,:,:]=np.divide(solution[n_step,tn,:,:],growth[n_step,tn,np.newaxis,:])
+    if tn % 10 == 0:
         np.memmap.flush(solution)
         np.memmap.flush(contracted_CLVs)
+        np.memmap.flush(normalized_solution)
         
 print("Saveing results in folder "+savename+".")
