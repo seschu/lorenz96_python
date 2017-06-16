c632d103c42445bb518af40f3a30c447ce59e9cb
Sebastian Schubert <sebastian.schubert@uni-hamburg.de>
2017-06-16 09:44:51 +0200

    v3

--- lorenz96_secondary_instabilities.py
+++ lorenz96_secondary_instabilities.py
@@ -20,7 +20,8 @@ paraL96 = {'F1' : 10,
 M = paraL96['dimX'] + paraL96['dimX']*paraL96['dimY'] # -1 full spectrum
 dimN = paraL96['dimX'] + paraL96['dimX']*paraL96['dimY'] # -1 full spectrum
 integrator = 'classic'
-t = np.arange(0,1000,0.1)
+dt = 0.01 #np.mean(np.diff(t))
+t = np.arange(0,1000,1)
 spinup = 100;
 #setup L96
 hs=[ 1. ] #   ,  0.0625,  0.125 ,  0.25  ,  0.5   ,  1.    ]
@@ -28,7 +29,7 @@ hs=[ 1. ] #   ,  0.0625,  0.125 ,  0.25  ,  0.5   ,  1.    ]
 
 testzeroclv=True
 
-savename='secondaryinstabilities_v3'
+savename='secondaryinstabilities_1layer'
 if not os.path.exists(savename): os.mkdir(savename)
 CLV = np.memmap(savename+'/CLV.dat',mode='w+',shape=(len(t),dimN,M,len(hs)),dtype='float64')
 BLV = np.memmap(savename+'/BLV.dat',mode='w+',shape=(len(t),dimN,M,len(hs)),dtype='float64')
@@ -41,7 +42,6 @@ np.save(savename+'/t',t)
 if testzeroclv: tendency = np.memmap(savename+'/tendency.dat',mode='w+',shape=(len(t),dimN,len(hs)),dtype='float64')
 if testzeroclv: tendcorr = np.memmap(savename+'/tendcorr.dat',mode='w+',shape=(len(t),len(hs)),dtype='float64')
 
-tendcorr = np.memmap(savename+'/tendcorr.dat',mode='r',shape=(len(t),len(hs)),dtype='float64')
 
 # Compute the exponents
 
@@ -59,7 +59,7 @@ for count,h in enumerate(hs):
     # spinup
     print("\nSpinup ...")
     #for i in range(0,int(spinup/0.1),1): 
-    field.integrate_back(spinup,dt=np.mean(np.diff(t)))
+    field.integrate_back(spinup,dt=dt)
     field.step_t = 0.0
 
     BLV[0,:,:,count]=field.x['lin']
@@ -67,7 +67,7 @@ for count,h in enumerate(hs):
     # Do QR step
     for tn, (ts,te) in enumerate(zip(t[0:-1],t[1:])):
         if testzeroclv: tendency[tn,:,count] = L96(ts,field.x['back'])
-        field.qr_step(te-ts,dt=np.mean(np.diff(t)))
+        field.qr_step(te-ts,dt=dt)
         R[tn,:,:,count]=field.R
         BLV[tn+1,:,:,count]=field.x['lin']
         print(te,'|',count+1,'/',len(hs))
@@ -87,7 +87,7 @@ for count,h in enumerate(hs):
     res=res/np.linalg.norm(res,axis=0,keepdims=True)
     lyaploc_clv[imax,:,count]=np.log(1/np.abs(np.linalg.norm(CLV[imax,:,:,count],axis=0)))/np.abs(te-ts)
 
-    if testzeroclv: minloc=np.argmin(np.abs(lyapmean_blv))-1
+    if testzeroclv: minloc=np.argmin(np.abs(lyapmean_blv))
         
     for tn, (ts,te) in enumerate(zip(t[-2:0:-1],t[-1:0:-1])):
         n = imax-tn
@@ -123,7 +123,9 @@ lowest=np.zeros((corrs.shape[0],M))
 length=np.zeros((corrs.shape[0],M))
 for n,c in enumerate(corrs):
     d = (np.logical_and(np.abs(tendcorr[:,0])>c,np.abs(tendcorr[:,0])<c+0.1))
-    length[n,:] = np.sum(d) 
-    lowest[n,:] = np.average(lyaploc_clv[:,:,0],weights = d, axis = 0)
-
+    if d.any(): 
+        length[n,:] = np.sum(d) 
+        lowest[n,:] = np.average(lyaploc_clv[:,:,0],weights = d, axis = 0)
+    else:
+        lowest[n,:] = 0
 maskcorr = (np.logical_and(np.abs(tendcorr[:,0])>0.95,np.abs(tendcorr[:,0])<1.01))
